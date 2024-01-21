1315cff970e54087b44952a9d0e614385219ff87
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2024-01-21 23:08:33 +0530

    Update email-validation.js

--- email-validation.js
+++ email-validation.js
@@ -1,7 +1,7 @@
 function ValidateEmail(inputText) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (inputText.value.match(mailformat)) {
-        alert("Valid email address!");
+        alert("Valid email address!!!");
         document.form1.text1.focus();
         return true;
     }
